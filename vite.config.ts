485ca5203edaecbeb125b0efc620c10066d7febc
Cameron McKay <camerondeveloper93@gmail.com>
2026-05-04 00:13:37 +0100

    fix: fix cname

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from "vite";
 
+const repositoryName = process.env.GITHUB_REPOSITORY?.split("/")[1];
+const githubPagesBase = repositoryName ? `/${repositoryName}/` : "/";
+
 // https://vite.dev/config/
 export default defineConfig({
+  base: process.env.GITHUB_ACTIONS ? githubPagesBase : "/",
   server: {
     port: 8080,
     open: true,
